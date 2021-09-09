0e7a39bec58ce6e0ebb90c309cf0108b4aac4704
genedan <genedan@gmail.com>
2021-09-08 20:59:56 -0500

    update blog title

--- blog_src/conf.py
+++ blog_src/conf.py
@@ -45,6 +45,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'furo'
+html_title = "Insurance Tycoon 2000 <br /> <br /> Development Blog"
+# html_logo = "_static/trianglelogo.svg"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
